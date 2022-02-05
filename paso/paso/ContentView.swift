26da0458e1ca29dedd2fb1a33361e02b80a3db55
Samuel Franusic <sfranusic@gmail.com>
2022-02-04 16:13:12 -0800

    Add xcode project

---
+++ paso/paso/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  paso
+//
+//  Created by Sammy Franusic on 2/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ paso/paso/pasoApp.swift
@@ -0,0 +1,17 @@
+//
+//  pasoApp.swift
+//  paso
+//
+//  Created by Sammy Franusic on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct pasoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

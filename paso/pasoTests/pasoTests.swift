70fee81b435ed518cbbf272503a360f77100d86f
Samuel Franusic <sfranusic@gmail.com>
2022-03-11 12:34:29 -0800

    Add unit test for alert status if health data is nil

--- paso/pasoTests/pasoTests.swift
+++ paso/pasoTests/pasoTests.swift
@@ -15,10 +15,21 @@ class pasoTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testHealthKitStepCount() async {
-        let hk = HealthKitHelper()
-        let steps: Double = await hk.stepCount
-        XCTAssert(steps >= 0)
+    func testFailureToGetStepsTriggersAlert() async throws {
+        let healthKitController = HealthKitController()
+        do {
+            // Property 'shouldAlert' is false by default.
+            XCTAssert(healthKitController.shouldAlert == false,
+                      "Controller's alert property should be false by default.")
+            // Without user interaction the permissions will not be changed,
+            // and the call to update the step count will fail as a result.
+            try await healthKitController.authorizeHealthKit()
+            try await healthKitController.updatePublishedStepCount()
+            XCTAssert(healthKitController.shouldAlert == true,
+                      "Controller's alert property should be true if step count is not read.")
+        } catch {
+            XCTFail()
+        }
     }
 
     func testExample() throws {
